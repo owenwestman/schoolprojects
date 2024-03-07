d75b2b32f2fe7ce0e77bd70f48a16db66da4f3ed
owenwestman <owen.eastman@augustaschools.org>
2024-03-07 13:20:55 +0000

    added indexSlicing.py

---
+++ indexSlicing.py
@@ -0,0 +1,10 @@
+# init variable
+papas = ["Pizzeria", "Freezeria", "Cupcakeria", "Burgeria", "Bakeria", "Cheeseria", "Taco Mia", "Pancakeria", "Scooperia", "Donuteria", "Hot Doggeria", "Sushiria"]
+#print list
+print(papas)
+# list length
+print(len(papas))
+# print 4th item (index starts at zero)
+print(papas[3])
+# print 2nd through 5th items
+print(papas[1:5])
